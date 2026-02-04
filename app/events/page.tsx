3d485b71d653e205a88e4bc8d5c827e62cb8ed8d
Jason Yoder <jason.yoder2@bsu.edu>
2026-02-04 09:49:36 -0500

    Add upcoming events

---
+++ app/events/page.tsx
@@ -0,0 +1,11 @@
+export default function EventsPage() {
+  return (
+    <section className="max-w-3xl mx-auto py-12 px-4 text-center">
+      <h1 className="text-4xl font-bold mb-6 text-black">Upcoming events</h1>
+
+      <p className="text-lg text-black leading-relaxed mb-6">
+        Kyle has no upcoming shows right now, check back later.
+      </p>
+    </section>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Kyle Schrock",
@@ -28,11 +18,10 @@ export default function RootLayout({
       <body className="min-h-screen bg-neutral-900 text-white flex flex-col">
         <header className="p-4 flex justify-between items-center bg-neutral-800 shadow">
           <div className="flex items-center space-x-3">
-            <img className="w-10 h-10 rounded-full object-cover" src="/kyle.jpg"></img>
-            <h1 className="text-xl font-bold">Kyle Schrock</h1>
+            <h1 className="text-xl font-bold"><Link href="/">Home</Link></h1>
           </div>
           <nav className="space-x-6">
-            <Link href="/">Media</Link>
+            <Link href="/events">Upcoming Shows</Link>
             <Link href="/about">About</Link>
             <Link href="/contact">Contact</Link>
           </nav>
